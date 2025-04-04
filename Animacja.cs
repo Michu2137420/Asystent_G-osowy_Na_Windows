0ebaa63fe3690c81c5f1b9ddeef68951182b7240
Michu <michal.romanczuk@poczta.onet.pl>
2025-04-04 13:34:17 +0200

    Add files via upload

---
+++ Animacja.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Threading;
+
+namespace MatrixAnimation
+{
+    class AnimacjaMatrix
+    {
+        public void DisplayAnimation()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.CursorVisible = false;
+
+            int width = Console.WindowWidth;
+            int height = Console.WindowHeight;
+            Random random = new Random();
+
+            // Definiujemy kształt liter w napisie "Monday"
+            string[] mShape = new string[]
+            {
+                "M       M",
+                "MM     MM",
+                "M M   M M",
+                "M  M M  M",
+                "M   M   M",
+                "M       M",
+                "M       M"
+            };
+
+            string[] oShape = new string[]
+            {
+                " OOOOO ",
+                "O     O",
+                "O     O",
+                "O     O",
+                "O     O",
+                "O     O",
+                " OOOOO "
+            };
+
+            string[] nShape = new string[]
+            {
+                "N     N",
+                "NN    N",
+                "N N   N",
+                "N  N  N",
+                "N   N N",
+                "N    NN",
+                "N     N"
+            };
+
+            string[] dShape = new string[]
+            {
+                "DDDDDD  ",
+                "D     D ",
+                "D     D ",
+                "D     D ",
+                "D     D ",
+                "D     D ",
+                "DDDDDD  "
+            };
+
+            string[] aShape = new string[]
+            {
+                "   A   ",
+                "  A A  ",
+                " A   A ",
+                "AAAAAAA",
+                "A     A",
+                "A     A",
+                "A     A"
+            };
+
+            string[] yShape = new string[]
+            {
+                "Y     Y",
+                " Y   Y ",
+                "  Y Y  ",
+                "   Y   ",
+                "   Y   ",
+                "   Y   ",
+                "   Y   "
+            };
+
+            string[][] letterShapes = new string[][] { mShape, oShape, nShape, dShape, aShape, yShape };
+            int letterWidth = 10;
+            int letterHeight = 7;
+            int totalWidth = letterWidth * 6;
+            int startX = (width - totalWidth) / 2;
+            int startY = height / 4;
+
+            // Tablica do śledzenia pozycji spadających liter w tle
+            int[] yPositionsBackground = new int[width];
+            for (int i = 0; i < width; i++)
+            {
+                yPositionsBackground[i] = random.Next(height); // Ustawiamy losową początkową pozycję
+            }
+
+            // Zmienna do śledzenia czasu trwania animacji
+            DateTime startTime = DateTime.Now;
+
+            bool wordFormed = false;
+            while (!wordFormed)
+            {
+                // Sprawdzamy, czy minęło 5 sekund
+                if ((DateTime.Now - startTime).TotalSeconds > 5)
+                {
+                    break;
+                }
+
+                Console.Clear();
+
+                // Rysujemy spadające litery w tle
+                for (int x = 0; x < width; x++)
+                {
+                    if (yPositionsBackground[x] < height)
+                    {
+                        Console.SetCursorPosition(x, yPositionsBackground[x]);
+                        Console.Write(GetRandomChar(random));
+                        yPositionsBackground[x]++;
+                    }
+                    else
+                    {
+                        yPositionsBackground[x] = 0; // Zresetowanie pozycji do góry po osiągnięciu końca
+                    }
+                }
+
+                // Rysujemy napis "Monday" na pierwszym planie
+                for (int i = 0; i < 6; i++)
+                {
+                    string[] shape = letterShapes[i];
+                    for (int j = 0; j < letterHeight; j++)
+                    {
+                        Console.SetCursorPosition(startX + i * letterWidth, startY + j);
+                        Console.WriteLine(shape[j]);
+                    }
+                }
+
+                // Sprawdzamy, czy wszystkie litery napisu "Monday" są już na ekranie
+                wordFormed = true;
+                foreach (int yPos in yPositionsBackground)
+                {
+                    if (yPos < height)
+                    {
+                        wordFormed = false;
+                        break;
+                    }
+                }
+
+                Thread.Sleep(10);
+            }
+
+            // Czekamy chwilę, aby użytkownik zobaczył wynik
+            Thread.Sleep(5000);
+        }
+
+        static char GetRandomChar(Random random)
+        {
+            string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            return chars[random.Next(chars.Length)];
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Globalization;
+using System.Speech.Recognition;
+using MatrixAnimation;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace SpeechRecognitionApp
+{
+    class Program
+    {
+        private bool Active = false;
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8; // Umożliwia wyświetlanie polskich znaków w konsoli
+            MatrixAnimation.AnimacjaMatrix Animacja = new MatrixAnimation.AnimacjaMatrix();
+            Program program = new Program();
+            Animacja.DisplayAnimation();
+            program.Initialization();
+            
+
+        }
+
+        private void Initialization()
+        {
+            LoadSpeechRecognition();
+        }
+
+        private void LoadSpeechRecognition()
+        {
+            try
+            {
+                // Create a SpeechRecognitionEngine instance with the desired culture
+                CultureInfo culture = new CultureInfo("en-US");
+                SpeechRecognitionEngine recognizer = new SpeechRecognitionEngine(culture);
+
+                // Create a Choices object with the commands
+                Choices commands = new Choices(new string[] { "open browser", "close application", "play music", "start", "off","open kompilator" });
+
+                // Create a GrammarBuilder and set its culture to match the recognizer
+                GrammarBuilder grammarBuilder = new GrammarBuilder();
+                grammarBuilder.Culture = culture;
+                grammarBuilder.Append("Monday");
+                grammarBuilder.Append(commands);
+
+                // Create a Grammar from the GrammarBuilder
+                Grammar grammar = new Grammar(grammarBuilder);
+
+                // Load the grammar into the recognizer
+                recognizer.LoadGrammar(grammar);
+
+                // Register the SpeechRecognized event handler
+                recognizer.SpeechRecognized += Recognizer_SpeechRecognized;
+
+                // Set the input to the default audio device
+                recognizer.SetInputToDefaultAudioDevice();
+
+                // Start asynchronous, continuous speech recognition
+                recognizer.RecognizeAsync(RecognizeMode.Multiple);
+
+                Console.WriteLine("\nAsystent 'Monday' jest aktywny. Powiedz 'Monday start', aby go uruchomić. Naciśnij Enter, aby zakończyć...");
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Błąd podczas inicjalizacji rozpoznawania mowy: " + ex.Message);
+            }
+        }
+
+        private void Recognizer_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            string command = e.Result.Text.ToLower().Replace("monday", "").Trim();
+
+            if (command == "start")
+            {
+                Active = true;
+                Console.WriteLine("Asystent Monday został aktywowany.");
+                Console.WriteLine("Możesz teraz wydawać polecenia.");
+                return;
+            }
+
+            if (command == "off")
+            {
+                Active = false;
+                Console.WriteLine("Asystent Monday został uśpiony.");
+                return;
+            }
+
+            if (!Active)
+            {
+                Console.WriteLine("Asystent Monday jest wyłączony lub uśpiony. Należy go włączyć za pomocą komendy 'Monday start'.");
+                return;
+            }
+
+            switch (command)
+            {
+                case "open browser":
+                    Console.WriteLine("Otwieranie przeglądarki...");
+                    OpenBrowser("https://www.google.com");
+                    break;
+                case "close application":
+                    Console.WriteLine("Zamykanie aplikacji...");
+                    CloseApplication();
+                    break;
+                case "play music":
+                    Console.WriteLine("Odtwarzanie muzyki...");
+                    OpenBrowser("https://open.spotify.com/");
+                    break;
+                case "open kompilator":
+                    Console.WriteLine("Otwieranie kompilatora...");
+                    var startInfo = new ProcessStartInfo
+                    {
+                        FileName = @"C:\Program Files (x86)\Dev-Cpp\devcpp.exe",
+                        UseShellExecute = true
+                    };
+                    Process.Start(startInfo);
+                    break;
+                default:
+                    Console.WriteLine("Nie rozpoznano polecenia.");
+                    break;
+            }
+            
+        }
+        private void OpenBrowser(string url)
+        {
+            try
+            {
+                Process.Start(url);
+            }
+            catch
+            {
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    url = url.Replace("&", "^&");
+                    Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                {
+                    Process.Start("xdg-open", url);
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                {
+                    Process.Start("open", url);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+
+        private void CloseApplication()
+        {
+            Environment.Exit(0);
+        }
+    }
+}
+
